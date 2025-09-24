aec38ea94317e0b1baf1a08571d57a9b390d9d2b
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-23 23:50:16 -0500

    See #8, Update sockets files, add new features and fixes!

--- src/sockets/socket_avl_tree.py
+++ src/sockets/socket_avl_tree.py
@@ -1,18 +1,24 @@
+from src.controllers.socket_avl_controller import SocketAVLController
 from src.models.tree import AVLTree
 from src.sockets.socket_manager import SocketManager
 
 class SocketAVLTree:
     def __init__(self, socketIO: SocketManager, tree_model: AVLTree):
-        # super().__init__(app, cors, "/AVLTree")
         self.socketio = socketIO
         self.tree = tree_model
-
+        self.controller: SocketAVLController = SocketAVLController(tree_model, self.socketio)
+        
+        # I get a problem, self.socket scure will be None or not ever update... Test...
         self.socketio.namespace = "/AVLTree"
         self.__register_handlers()
+
+
 
     def __register_handlers(self):
         self.socketio.register_handler(False, 'connect', 'connected')
         self.socketio.register_handler(True, 'disconnect', 'disconnected')
         # Insert obstacle, receive (msg, response) because i don't send any *args
         # I Think!
-        self.socketio.register_handler(False, "insert_obstacle", "obstacles_inserted", self.tree.setObstacle)
+        self.socketio.register_handler(False, "insert_obstacle", "obstacles_inserted", self.controller.emit_obstacle_inserted, True)
+        # self.socketio.register_handler(False, "insert_obstacle", "obstacles_inserted", self.tree.setObstacle)
+        self.socketio.register_handler(False, "road_inorder", "inorder", self.controller.emit_road_inorder, True)
--- src/sockets/socket_manager.py
+++ src/sockets/socket_manager.py
@@ -14,18 +14,28 @@ class SocketManager(SocketIO):
         # Register the basics handlers (Connect & Disconnect)
         self.__register_basics_handlers()
 
-    def register_handler(self, not_channel = True, event_name: str = '', emit_name: str = '', callback = None, *callback_args):
+    def register_handler(self, not_channel = True, event_name: str = '', emit_name: str = '', callback: Any | None = None, need_control_emit: bool = False, *callback_args):
         # Call the handle_event()
         # When are the BASIC handlers, the namespace is "/"
         __namespace = "/" if not_channel else self.namespace
-        self.on_event(event_name, lambda msg: self.handle_event(msg, emit_name, callback, *callback_args), namespace=__namespace)
+        self.on_event(event_name, lambda msg: self.handle_event(msg, emit_name, callback, need_control_emit, *callback_args), namespace=__namespace)
 
     def __register_basics_handlers(self) -> None:
-        self.register_handler(True, 'connect', 'connected')
-        self.register_handler(True, 'disconnect', 'disconnected')
+        self.register_handler(True, 'connect', 'connected', self.__on_connect, True)
+        self.register_handler(True, 'disconnect', 'disconnected', self.__on_disconnect, True)
 
-    def handle_event(self, msg: Any, emit_name: str, callback = None, *calback_args) -> None:
-        # By default, response is {True, DataReceived}
+    def __on_connect(self, msg: Any, response: ResponseSocket, emit_name: str, namespace: str) -> None:
+        response.msg = 'The Socket is Connected, ready to use!'
+        emit(emit_name, response.to_dict(), namespace=namespace)
+
+    def __on_disconnect(self, msg: Any, response: ResponseSocket, emit_name: str, namespace: str) -> None:
+        response.msg = 'The has been disconneted!'
+        emit(emit_name, response.to_dict(), namespace=namespace)
+        # Stop socket!
+        self.stop()
+
+    def handle_event(self, msg: Any, emit_name: str, callback: Any | None = None, need_control_emit: bool = False, *calback_args) -> None:
+        # By default, response is {True, DataReceived, Eventname -> channel/endpoint, EmitName -> Endpoint}
         # After can changes with the callback
         response = ResponseSocket(True, msg)
 
@@ -33,9 +43,17 @@ class SocketManager(SocketIO):
         # Ever send the response reference to modify it's values, too send msg, data received!!!
         if callback:
             if calback_args:
-                callback(msg, response, *calback_args)
+                # Callback said that need the control of EMIT method
+                if need_control_emit:
+                    callback(msg, response, emit_name, self.namespace, *calback_args)
+                else:
+                    callback(msg, response*calback_args)
             else:
-                callback(msg, response)
+                if need_control_emit:
+                    callback(msg, response, emit_name, self.namespace)
+                else:
+                    callback(msg, response)
         
-        # Emit event to client, said that event is handled OK! or !OK
-        emit(emit_name, response.to_dict(), namespace=self.namespace)
+        if not need_control_emit:
+            # Emit event to client, said that event is handled OK! or !OK
+            emit(emit_name, response.to_dict(), namespace=self.namespace)
