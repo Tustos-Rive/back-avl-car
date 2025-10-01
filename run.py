4979a96c1aa2879280bb97d4a407dd9bee468899
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-30 22:43:59 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-from src.main import Main
-from random import randint
-
-import gevent.monkey
-
-from src.models.obstacle import Obstacle
-from src.models.tree import AVLTree
-from src.utils.others_utils import Utils
+import os
+from src.main import app
 
 if __name__ == "__main__":
-    gevent.monkey.patch_all()
-    main = Main("*")
-    main.socketio.run(main.app, host='localhost', port=4500, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.getenv("PORT", 4500))  # Fallback to 4500 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
