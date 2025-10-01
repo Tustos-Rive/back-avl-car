d5708da4ad6f376c4e8837883d34d2c3fa27fc3a
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-30 22:18:30 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from src.main import Main
 from random import randint
 
+import gevent.monkey
+
 from src.models.obstacle import Obstacle
 from src.models.tree import AVLTree
 from src.utils.others_utils import Utils
 
 if __name__ == "__main__":
+    gevent.monkey.patch_all()
     main = Main("*")
     main.socketio.run(main.app, host='localhost', port=4500, debug=True, allow_unsafe_werkzeug=True)
