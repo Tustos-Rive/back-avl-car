a883b43ad7fc04e2b1edbf201dea23c504f4c7ee
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-30 21:35:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ from src.utils.others_utils import Utils
 
 if __name__ == '__main__':
     main = Main("*")
-    main.socketio.run(main.app, host='localhost', port=4500, debug=True)
+    main.socketio.run(main.app, host='localhost', port=4500, debug=True, allow_unsafe_werkzeug=True)
