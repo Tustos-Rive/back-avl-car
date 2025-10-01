92787282ad601d611e230aa9884218259cfeef1b
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-30 21:52:11 -0500

    Update run.py

--- run.py
+++ run.py
@@ -6,8 +6,4 @@ from src.models.tree import AVLTree
 from src.utils.others_utils import Utils
 
 if __name__ == "__main__":
-    import sys
-    if "--serve" in sys.argv:
-        main.socketio.run(main.app, host="0.0.0.0", port=4500, debug=True, allow_unsafe_werkzeug=True)
-    else:
-        print("Build mode: not starting Flask server")
+    main.socketio.run(main.app, host="0.0.0.0", port=4500, debug=True, allow_unsafe_werkzeug=True)
