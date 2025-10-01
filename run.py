52789e8aa5942a17fa04b372cf5a2fa7786b02b7
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-30 22:49:47 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from src.main import Main
 if __name__ == "__main__":
     main = Main('*')
     port = int(os.getenv("PORT", 4500))  # Fallback to 4500 if PORT is not set
-    main.socketio.run(host="0.0.0.0", port=port)
+    main.socketio.run(main.app, host="0.0.0.0", port=port,debug=True)
